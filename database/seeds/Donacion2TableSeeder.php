0a542dd280456233ece48909a188a460f52e453f
Andrea9730 <andrea00aguiar@gmail.com.com>
2021-03-07 22:28:55 -0300

    seeds

--- database/seeds/Donacion2TableSeeder.php
+++ database/seeds/Donacion2TableSeeder.php
@@ -11,6 +11,23 @@ class Donacion2TableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('donacion_2')->delete();
+        $json = File::get(__DIR__ . '/json/cucaimis.json');
+        $data = json_decode($json);
+          
+        foreach ($data as $item){
+              Donacion2::create(array(
+                'id_proceso'=> $item ->idProceso,
+                'entrevista_fam'=> $item ->entrevistaFam,
+                'orden_judicial'=> $item -> ordenJudicial,
+                'disp_ablacionista' => $item -> dispAblacionista,
+                'envio' => $item ->envio,
+                'fecha_ablacion' => $item -> fechaAblacion,
+                'logistica' => $item -> logistica,
+                'distribucion' => $item -> distribucion,
+    
+                  
+          ));
+        }
     }
 }
