aa54bd77f10a86e1c4ddab615ca92c95dfc6318c
Andrea9730 <andrea00aguiar@gmail.com.com>
2021-03-07 21:40:24 -0300

    seeds

---
+++ database/seeds/UsuariosTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsuariosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
